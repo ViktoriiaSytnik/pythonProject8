52e8ffa7eaa92cbdb85936628e1e7ec091385559
ViktoriiaSytnik <ssytnik@kse.org.ua>
2023-01-14 08:11:25 +0100

    paddle, ball, background

--- main.py
+++ main.py
@@ -1,16 +1,38 @@
 import pgzrun
+import random
 
+from pgzero import screen
+from pgzero.actor import Actor
+
+TITLE = "Christmas Breakout"
 WIDTH = 600
-HEIGHT = 800
+HEIGHT = 500
+
+paddle = Actor("paddleblue.png")
+paddle.x = 120
+paddle.y = 420
+
+ball = Actor("ballblue.png")
+ball.x = 30
+ball.y = 300
+
+paddle.x = 120
+paddle.y = 420
 
 
 def draw():
-    pass
+    paddle.draw()
+    ball.draw()
 
 
-def update(dt):
-    pass
+# ball physics
+def update():
+    update_ball()
+
+
+def update_ball():
+    ball.x -= 1
+    ball.y -= 1
 
 
 pgzrun.go()
-
