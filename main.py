1e7efe7fd58abfe81eec0402157a5ef551aadcd2
ViktoriiaSytnik <ssytnik@kse.org.ua>
2023-01-15 10:50:07 +0100

    classes added

--- main.py
+++ main.py
@@ -1,66 +1,43 @@
 import pgzrun
 import random
-
-from pgzero import screen
 from pgzero.actor import Actor
-from pgzero.constants import mouse
-from pgzero.keyboard import keyboard
+import pygame
 
-TITLE = "Christmas Breakout"
 WIDTH = 600
 HEIGHT = 600
-
-paddle = Actor("paddleblue.png")
-paddle.x = 120
-paddle.y = 420
-
-ball = Actor("ballblue.png")
-ball.x = 30
-ball.y = 300
-
-ball_x_speed = -1
-ball_y_speed = -1
-
-paddle.x = 120
-paddle.y = 420
+TITLE = "Christmas Breakout"
 
 
-def draw():
-    paddle.draw()
-    ball.draw()
+class Paddle:
+    def __init__(self):
+        self.actor = Actor("paddleblue.png", center=(WIDTH // 2, HEIGHT - 30))
+
+    def draw(self):
+        self.actor.draw()
 
 
-# ball physics
+class Ball:
+    def __init__(self, speed: int):
+        self.actor = Actor('ballblue.png', center=(WIDTH // 2, HEIGHT // 2))
+        self.speed = speed
+        self.ball_dx = self.speed
+        self.ball_dy = self.speed
+        self.radius = 20
 
-def update():
-    global ball_x_speed, ball_y_speed
-    if keyboard.left:
-        paddle.x = paddle.x - 5
-    if keyboard.right:
-        paddle.x = paddle.x + 5
+    def update(self):
+        self.actor.x += self.ball_dx
+        self.actor.y += self.ball_dy
 
-
-if paddle.colliderect(ball):
-    ball_y_speed *= -1
-    # randomly move ball left or right on hit
-    rand = random.randint(0, 1)
-    if rand:
-        ball_x_speed *= -1
+    def draw(self):
+        self.actor.draw()
 
 
-def update_ball():
-    global ball_x_speed, ball_y_speed
-    ball.x -= ball_x_speed
-    ball.y -= ball_y_speed
-    if (ball.x >= WIDTH) or (ball.x <= 0):
-        ball_x_speed *= -1
-    if (ball.y >= HEIGHT) or (ball.y <= 0):
-        ball_y_speed *= -1
+paddle = Paddle()
 
 
 def on_mouse_move(pos):
     x, y = pos
-    paddle.x = x
+    paddle.actor.x = x
 
 
 pgzrun.go()
